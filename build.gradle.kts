560a1ebb1fb2c53b5b36808e6b025ae1bacb42e4
Chris Matheny <cmatheny@asu.edu>
2021-01-07 23:20:45 -0700

    Add shadowJar and check-updates, update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.21"
+    kotlin("jvm") version "1.4.21-2"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+    id("name.remal.check-updates") version "1.1.6"
 }
 
 group = "me.cmatheny"
@@ -13,14 +15,20 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test-junit5"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
+tasks {
+    test {
+        useJUnitPlatform()
+    }
 
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
+    withType<KotlinCompile>() {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    withType<GradleBuild> {
+        finalizedBy(shadowJar)
+    }
 }
